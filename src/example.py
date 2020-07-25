a67dbb609e470b903bd6f1161d303a7f2f147a10
Kyo Kutsuzawa <kutsuzawa@tohoku.ac.jp>
2020-07-25 22:37:49 +0900

    Edit example.py

--- src/example.py
+++ src/example.py
@@ -1,26 +1,20 @@
+# Import tuiplot.py
 import tuiplot
 
+# Create data
+import numpy as np
+x = np.linspace(0, 5, 100, endpoint=True)
+y1 = np.sin(x * 0.2 * 2*np.pi)
+y2 = np.cos(x * 0.2 * 2*np.pi)
+y3 = x * 0.1
 
-def example():
-    import numpy as np
+# Initialize a figure
+fig = tuiplot.Figure()
 
-    # Create data
-    x = np.linspace(0, 5, 100, endpoint=True)
-    y1 = np.sin(x * 0.2 * 2*np.pi)
-    y2 = np.cos(x * 0.2 * 2*np.pi)
-    y3 = x * 0.1
+# Add data to the figure
+fig.plot(x, y1, label='sin(2pi * 0.2 x)')
+fig.plot(x, y2, label='cos(2pi * 0.2 x)')
+fig.plot(x, y3, label='0.1 x')
 
-    # Initialize a figure
-    fig = tuiplot.Figure()
-
-    # Add data to the figure
-    fig.plot(x, y1, label='sin(2pi * 0.2 x)')
-    fig.plot(x, y2, label='cos(2pi * 0.2 x)')
-    fig.plot(x, y3, label='0.1 x')
-
-    # Show the figure
-    fig.show()
-
-
-if __name__ == "__main__":
-    example()
+# Show the figure
+fig.show()
